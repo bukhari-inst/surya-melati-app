74856f98faee40e716729bffb932ad948b40429e
Bukhari_Inst <bukhari414647@gmail.com>
2022-08-31 16:06:44 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,18 +2,19 @@
 
 namespace App\Controllers;
 
-// use App\Models\ModelPasien;
+use App\Models\ModelPasien;
 
 class Home extends BaseController
 {
     public function __construct()
     {
-        // $this->ModelPasien = new ModelPasien();
+        $this->ModelPasien = new ModelPasien();
     }
 
     public function index()
     {
-        // $pasien = $this->ModelPasien->findAll();
+        $pasien = $this->ModelPasien->findAll();
+
         // dd($pasien);
 
         $data = [];
