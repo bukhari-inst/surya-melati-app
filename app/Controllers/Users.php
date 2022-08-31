f7fd0d28b10c187a723887d3b0609a542c14a38b
Bukhari_Inst <bukhari414647@gmail.com>
2022-08-31 16:06:46 +0700

    Update Users.php

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -2,16 +2,41 @@
 
 namespace App\Controllers;
 
+use App\Models\ModelPasien;
+
 class Users extends BaseController
 {
     public function __construct()
     {
+        $this->ModelPasien = new ModelPasien();
     }
 
     public function login()
     {
-        $data = [];
-
         return view('pages/auth/login');
+    }
+
+    public function loginProcess()
+    {
+        $noRkmMedis = $this->request->getPost('noRkmMedis');
+        $date = $this->request->getVar('date');
+        $user = $this->ModelPasien->getPasienWhereNoRkmMedis($noRkmMedis);
+
+        if ($user) {
+            if ($date == $user->tgl_lahir) {
+                $params = ['id_user' => $user->no_rkm_medis];
+                session()->set($params);
+                return redirect()->to(site_url('/pendaftaran'));
+            } else {
+                return redirect()->back()->with('error', 'Tanggal lahir tidak sesuai');
+            }
+        } else {
+            return redirect()->back()->with('error', 'Nomor rekam medik tidak ditemukan');
+        }
+    }
+
+    public function pendaftaran()
+    {
+        return view('pages/users/index');
     }
 }
