fe1e8ff3c28a411c890d3ad3a2e33c97206b6bef
Bukhari_Inst <bukhari414647@gmail.com>
2022-08-31 16:06:38 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,8 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/login', 'Users::login');
+$routes->post('/loginProcess', 'Users::loginProcess');
+$routes->get('/pendaftaran', 'Users::pendaftaran');
 
 /*
  * --------------------------------------------------------------------
