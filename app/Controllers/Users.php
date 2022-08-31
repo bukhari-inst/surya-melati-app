dd5ce912b6c74aa9f681b97603621f2b541d1cd6
Bukhari_Inst <bukhari414647@gmail.com>
2022-08-31 09:15:22 +0700

    Create Users.php

---
+++ app/Controllers/Users.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Users extends BaseController
+{
+    public function __construct()
+    {
+    }
+
+    public function login()
+    {
+        dd("sukses");
+    }
+}
