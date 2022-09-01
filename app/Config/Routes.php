ac012445621a1b87c30f96246b8f218439355256
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-01 13:04:13 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,8 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+$routes->get('/', 'Users::pendaftaran');
 $routes->get('/login', 'Users::login');
 $routes->post('/loginProcess', 'Users::loginProcess');
 $routes->get('/pendaftaran', 'Users::pendaftaran');
