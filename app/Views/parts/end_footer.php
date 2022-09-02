89fc30f755b532f1a2c39a18a80b46ff60978ec9
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-02 16:03:11 +0700

    Update end_footer.php

--- app/Views/parts/end_footer.php
+++ app/Views/parts/end_footer.php
@@ -6,7 +6,35 @@
 <script src="<?= base_url(); ?>/assets/lib/waypoints/waypoints.min.js"></script>
 <script src="<?= base_url(); ?>/assets/lib/owlcarousel/owl.carousel.min.js"></script>
 <script src="<?= base_url(); ?>/assets/lib/counterup/counterup.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js" type="text/javascript">
+</script>
+<script>
+function getFieldDataPendaftaran() {
+    $('#tanggalkunjungan').change(function(e) {
+        $.ajax({
+            type: "post",
+            url: "<?= site_url('users/getPoliklinik') ?>",
+            data: {
+                poliklinik: $(this).val()
+            },
+            dataType: "json",
+            success: function(response) {
 
+            },
+            error: function(xhr, thrownError) {
+                alert(xhr.status + " \ n " + xhr.responseText + " \ n " + thrownError);
+            }
+        });
+    });
+}
+
+$(document).ready(function() {
+    getFieldDataPendaftaran();
+    $('.js-example-basic-single').select2({
+        width: 'resolve'
+    });
+});
+</script>
 <!-- Template Javascript -->
 <script src="<?= base_url(); ?>/assets/js/main.js"></script>
 </body>
