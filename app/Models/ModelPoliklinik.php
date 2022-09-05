944417758d7b536e98701603e236c5a609a03761
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-05 11:51:20 +0700

    Update ModelPoliklinik.php

--- app/Models/ModelPoliklinik.php
+++ app/Models/ModelPoliklinik.php
@@ -6,4 +6,22 @@ use CodeIgniter\Model;
 
 class ModelPoliklinik extends Model
 {
+    protected $table = 'poliklinik';
+    protected $primaryKey = 'kd_poli';
+
+    protected $returnType = 'object';
+
+    protected $allowedFields = [
+        'nm_poli',
+    ];
+
+    public function getPoliWhereDay($day = false)
+    {
+        return $this->select('jdwl.kd_poli,poliklinik.nm_poli, jdwl.jam_mulai, jdwl.jam_selesai')
+            ->join('jadwal jdwl', 'poliklinik.kd_poli = jdwl.kd_poli')
+            ->join('dokter dktr', 'dktr.kd_dokter = jdwl.kd_dokter')
+            ->where('jdwl.hari_kerja', $day)
+            ->groupBy('poliklinik.kd_poli')
+            ->findAll();
+    }
 }
