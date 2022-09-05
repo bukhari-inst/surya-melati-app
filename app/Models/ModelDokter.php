975a4b5d9aa14b5a47de65d83d16bfb8a84fe4c0
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-05 14:35:10 +0700

    Update ModelDokter.php

--- app/Models/ModelDokter.php
+++ app/Models/ModelDokter.php
@@ -6,4 +6,22 @@ use CodeIgniter\Model;
 
 class ModelDokter extends Model
 {
+    protected $table = 'dokter';
+    protected $primaryKey = 'kd_dokter';
+
+    protected $returnType = 'object';
+
+    protected $allowedFields = [
+        'nm_dokter',
+    ];
+
+    public function getDokterWherePoli($poli = false)
+    {
+        return $this->select('jdwl.kd_dokter, dktr.nm_dokter')
+            ->join('jadwal jdwl', 'dokter.kd_dokter = jdwl.kd_dokter')
+            ->join('dokter dktr', 'dktr.kd_dokter = jdwl.kd_dokter')
+            ->where('jdwl.kd_poli', $poli)
+            ->groupBy('dktr.nm_dokter')
+            ->findAll();
+    }
 }
