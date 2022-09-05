1a77e9cf55ea06bb6f567ae26472fb703e6f6c9a
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-05 13:36:29 +0700

    Update ModelPoliklinik.php

--- app/Models/ModelPoliklinik.php
+++ app/Models/ModelPoliklinik.php
@@ -17,7 +17,7 @@ class ModelPoliklinik extends Model
 
     public function getPoliWhereDay($day = false)
     {
-        return $this->select('jdwl.kd_poli,poliklinik.nm_poli, jdwl.jam_mulai, jdwl.jam_selesai')
+        return $this->select('jdwl.kd_poli,poliklinik.nm_poli, DATE_FORMAT(jdwl.jam_mulai, "%h:%i %p") as jam_mulai, DATE_FORMAT(jdwl.jam_selesai, "%h:%i %p") as jam_selesai')
             ->join('jadwal jdwl', 'poliklinik.kd_poli = jdwl.kd_poli')
             ->join('dokter dktr', 'dktr.kd_dokter = jdwl.kd_dokter')
             ->where('jdwl.hari_kerja', $day)
