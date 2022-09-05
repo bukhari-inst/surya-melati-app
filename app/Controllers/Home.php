f0c8cb167922b544dff65c9dbaeb51a5bb513bcd
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-05 15:48:27 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,21 +3,25 @@
 namespace App\Controllers;
 
 use App\Models\ModelPasien;
+use App\Models\ModelCaraBayar;
 
 class Home extends BaseController
 {
     public function __construct()
     {
         $this->ModelPasien = new ModelPasien();
+        $this->ModelCaraBayar = new ModelCaraBayar();
     }
 
     public function index()
     {
         $userId = session('id_user');
         $user = $this->ModelPasien->getPasienWhereNoRkmMedis($userId);
+        $payment = $this->ModelCaraBayar->getSpecificPay();
 
         $data = [
             'user' => $user,
+            'payment' => $payment,
         ];
 
         return view('pages/users/pendaftaran', $data);
