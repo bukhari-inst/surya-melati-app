48c7a5b6dcbba718d2770554b339c929dbde919a
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-05 15:18:57 +0700

    Create ModelCaraBayar.php

---
+++ app/Models/ModelCaraBayar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelCaraBayar extends Model
+{
+    protected $table = 'dokter';
+    protected $primaryKey = 'kd_pj';
+
+    protected $returnType = 'object';
+
+    protected $allowedFields = [
+        'png_jawab',
+    ];
+
+    public function getSpecificPay()
+    {
+        return $this->select('kd_pj, png_jawab')
+            ->like('png_jawab', 'BPJS')
+            ->orLike('png_jawab', 'BPJS KETENAGAKERJAAN')
+            ->orLike('png_jawab', 'JASA RAHARJA')
+            ->orLike('png_jawab', 'HALODOC')
+            ->findAll();
+    }
+}
