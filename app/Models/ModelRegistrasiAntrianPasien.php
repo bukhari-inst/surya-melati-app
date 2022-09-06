281de05b17f0e5a94ce1d7cf0556704588718726
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-06 16:00:51 +0700

    Update ModelRegistrasiAntrianPasien.php

--- app/Models/ModelRegistrasiAntrianPasien.php
+++ app/Models/ModelRegistrasiAntrianPasien.php
@@ -15,4 +15,12 @@ class ModelRegistrasiAntrianPasien extends Model
         'no_reg', 'no_rawat ', 'tgl_registrasi', 'jam_reg', 'kd_dokter', 'no_rkm_medis', 'kd_poli', 'almt_pj', 'hubunganpj',
         'biaya_reg', 'stts', 'stts_daftar', 'status_lanjut', 'kd_pj', 'umurdaftar', 'sttsumur', 'status_bayar',
     ];
+
+    public function getLastNoRegWhereDokterAndTglReg($dokter = false, $tanggalRreg = false)
+    {
+        return $this->selectMax('no_reg')
+            ->where('kd_dokter', $dokter)
+            ->where('tgl_registrasi', $tanggalRreg)
+            ->first();
+    }
 }
