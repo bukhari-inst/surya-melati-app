436057d30501c0bd77ef7ca28760edae8efb0e12
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-09 16:04:53 +0700

    Update navbar.php

--- app/Views/parts/navbar.php
+++ app/Views/parts/navbar.php
@@ -1,6 +1,6 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5">
-    <a href="index.html" class="navbar-brand d-flex align-items-center">
+    <a href="<?= base_url('/'); ?>" class="navbar-brand d-flex align-items-center">
         <h1 class="m-0">
             <img class="img-fluid me-3" src="<?= base_url(); ?>/assets/img/surya-melati.png" alt="" />Surya melati
         </h1>
@@ -10,10 +10,19 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto bg-light rounded pe-4 py-3 py-lg-0">
-            <a href="index.html" class="nav-item nav-link <?= uri_string() == '/' ? 'active' : '' ?>">Pendaftaran</a>
-            <a href="<?= base_url('/riwayatPeriksa'); ?>"
-                class="nav-item nav-link <?= uri_string() == '/riwayatPeriksa' || '/antrianSekarang' ? 'active' : '' ?>">Riwayat
-                Periksa</a>
+            <a href="<?= base_url('/'); ?>"
+                class="nav-item nav-link <?= uri_string() == '/' ? 'active' : '' ?>">Pendaftaran</a>
+            <div class="nav-item dropdown">
+                <a href="<?= base_url('/riwayatPeriksa'); ?>"
+                    class="nav-link dropdown-toggle <?= uri_string() == 'antrianSekarang' ? 'active' : '' ?>"
+                    data-bs-toggle="dropdown">Riwayat
+                    Periksa</a>
+                <div class="dropdown-menu bg-light border-0 m-0">
+                    <a href="<?= base_url('/riwayatPeriksa'); ?>" class="dropdown-item">Riwayat
+                        Periksa</a>
+                    <a href="<?= base_url('/antrianSekarang'); ?>" class="dropdown-item">Antrian Sekarang</a>
+                </div>
+            </div>
             <a href="<?= base_url('/jadwalDokter'); ?>"
                 class="nav-item nav-link <?= uri_string() == '/jadwalDokter' ? 'active' : '' ?>">Jadwal Dokter</a>
             <a href="<?= base_url('/informasiKamar'); ?>"
