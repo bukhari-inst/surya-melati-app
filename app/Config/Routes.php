5f7ebfd298e12fe8c32dfa44aa20c0e733700353
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-14 15:19:27 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -43,6 +43,8 @@ $routes->add('/getPoliklinik', 'Users::getPoliklinik');
 $routes->add('/getDokter', 'Users::getDokter');
 $routes->post('/registrasiAntrian', 'Users::registrasiAntrian');
 $routes->get('/antrianSekarang', 'Users::antrianSekarang');
+$routes->post('/gantiTanggalAntrian', 'Users::gantiTanggalAntrian');
+$routes->post('/batalTanggalAntrian', 'Users::batalTanggalAntrian');
 
 /*
  * --------------------------------------------------------------------
