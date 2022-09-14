3cf267efb16e881fb25b1dfa6242736040c6bda3
Bukhari_Inst <bukhari414647@gmail.com>
2022-09-14 15:19:32 +0700

    Update ModelRegistrasiAntrianPasien.php

--- app/Models/ModelRegistrasiAntrianPasien.php
+++ app/Models/ModelRegistrasiAntrianPasien.php
@@ -7,12 +7,12 @@ use CodeIgniter\Model;
 class ModelRegistrasiAntrianPasien extends Model
 {
     protected $table = 'reg_periksa';
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'no_rawat';
 
     protected $returnType = 'object';
 
     protected $allowedFields = [
-        'id', 'no_reg', 'no_rawat', 'tgl_registrasi', 'jam_reg', 'kd_dokter', 'no_rkm_medis', 'kd_poli', 'almt_pj', 'hubunganpj', 'biaya_reg', 'stts', 'stts_daftar', 'status_lanjut', 'kd_pj', 'umurdaftar', 'sttsumur', 'status_bayar'
+        'no_rawat', 'no_reg', 'tgl_registrasi', 'jam_reg', 'kd_dokter', 'no_rkm_medis', 'kd_poli', 'almt_pj', 'hubunganpj', 'biaya_reg', 'stts', 'stts_daftar', 'status_lanjut', 'kd_pj', 'umurdaftar', 'sttsumur', 'status_bayar'
     ];
 
     public function getLastNoRegWhereDokterAndTglReg($dokter = false, $tanggalRreg = false)
@@ -29,12 +29,27 @@ class ModelRegistrasiAntrianPasien extends Model
 
     public function getLastAntrian($userId = false)
     {
-        return $this->select('nm_pasien, reg_periksa.no_rkm_medis, tgl_registrasi, jam_reg, no_rawat, no_reg, nm_poli, nm_dokter, status_lanjut, png_jawab, status_bayar')
+        return $this->select('nm_pasien, reg_periksa.no_rkm_medis, tgl_registrasi, jam_reg, no_rawat, no_reg, nm_poli, nm_dokter, status_lanjut, png_jawab, status_bayar, DATE_FORMAT(jdwl.jam_mulai, "%h:%i %p") as jam_mulai, DATE_FORMAT(jdwl.jam_selesai, "%h:%i %p") as jam_selesai')
             ->join('poliklinik plk', 'reg_periksa.kd_poli = plk.kd_poli')
+            ->join('jadwal jdwl', 'plk.kd_poli = jdwl.kd_poli')
             ->join('dokter dktr', 'dktr.kd_dokter = reg_periksa.kd_dokter')
             ->join('penjab pnj', 'pnj.kd_pj = reg_periksa.kd_pj')
             ->join('pasien psn', 'psn.no_rkm_medis = reg_periksa.no_rkm_medis')
             ->where('reg_periksa.no_rkm_medis', $userId)
+            ->orderBy('reg_periksa.tgl_registrasi', 'DESC')
+            ->first();
+    }
+
+    public function getLastAntrianSttsBelum($userId = false)
+    {
+        return $this->select('nm_pasien, reg_periksa.no_rkm_medis, tgl_registrasi, jam_reg, no_rawat, no_reg, nm_poli, nm_dokter, status_lanjut, png_jawab, status_bayar, DATE_FORMAT(jdwl.jam_mulai, "%h:%i %p") as jam_mulai, DATE_FORMAT(jdwl.jam_selesai, "%h:%i %p") as jam_selesai')
+            ->join('poliklinik plk', 'reg_periksa.kd_poli = plk.kd_poli')
+            ->join('jadwal jdwl', 'plk.kd_poli = jdwl.kd_poli')
+            ->join('dokter dktr', 'dktr.kd_dokter = reg_periksa.kd_dokter')
+            ->join('penjab pnj', 'pnj.kd_pj = reg_periksa.kd_pj')
+            ->join('pasien psn', 'psn.no_rkm_medis = reg_periksa.no_rkm_medis')
+            ->where('reg_periksa.no_rkm_medis', $userId)
+            ->where('reg_periksa.stts', 'Belum')
             ->orderBy('reg_periksa.tgl_registrasi', 'DESC')
             ->first();
     }
