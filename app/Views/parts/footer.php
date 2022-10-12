5bd4201acc9b1b70a2f540fffe83a2ec115b7589
Bukhari_Inst <bukhari414647@gmail.com>
2022-10-12 13:29:45 +0700

    Update footer.php

--- app/Views/parts/footer.php
+++ app/Views/parts/footer.php
@@ -12,10 +12,15 @@
                     membantu Anda mendapatkan layanan berkualitas dan tetap sehat.
                 </p>
                 <div class="d-flex pt-2">
-                    <a class="btn btn-square me-1" href="https://www.instagram.com/rsum.surya.melati/" target="blank"><i class="fab fa-instagram"></i></a>
-                    <a class="btn btn-square me-1" href="https://www.tiktok.com/@rsumsuryamelati" target="blank"><img src="<?= base_url(); ?>/assets/img/icon/tiktok.svg" style="height: 16px"></a>
-                    <a class="btn btn-square me-1" href="https://www.facebook.com/rsum.surya.melati" target="blank"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-square me-0" href="https://www.youtube.com/channel/UC6mKOmvylyveO12NtsRTNoA/featured" target="blank"><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-square me-1" href="https://www.instagram.com/rsum.surya.melati/" target="blank"><i
+                            class="fab fa-instagram"></i></a>
+                    <a class="btn btn-square me-1" href="https://www.tiktok.com/@rsumsuryamelati" target="blank"><img
+                            src="<?= base_url(); ?>/assets/img/icon/tiktok.svg" style="height: 16px"></a>
+                    <a class="btn btn-square me-1" href="https://www.facebook.com/rsum.surya.melati" target="blank"><i
+                            class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square me-0"
+                        href="https://www.youtube.com/channel/UC6mKOmvylyveO12NtsRTNoA/featured" target="blank"><i
+                            class="fab fa-youtube"></i></a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6">
@@ -23,7 +28,7 @@
                 <div class="d-flex flex-row align-top">
                     <i class="fa fa-map-marker-alt me-3 pt-1"></i>
                     <p>Jl. Ngadiluwih - Wates Km. 10, Desa Ngletih, Kandat 64173,
-                    Kediri, Jawa Timur</p>
+                        Kediri, Jawa Timur</p>
                 </div>
                 <div class="d-flex flex-row">
                     <i class="fa fa-phone-alt me-3 pt-2"></i>
